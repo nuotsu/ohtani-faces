11fcfd5e3b9ab615f8486eef4802dd8ca29f563f
Mitchell Christ <xzmaru@gmail.com>
2021-10-29 01:15:06 -0700

    rename gettyEmbed to embed

--- schemas/sticker.js
+++ schemas/sticker.js
@@ -14,7 +14,7 @@ export default {
 				{ name: 'face', type: 'image', },
 				{ name: 'original', type: 'image', },
 				{ name: 'source', type: 'url', },
-				{ name: 'gettyEmbed', type: 'text', rows: 2, },
+				{ name: 'embed', type: 'text', rows: 2, },
 			],
 		},
 		{
