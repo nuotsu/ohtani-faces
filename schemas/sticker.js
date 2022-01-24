a2a26db610a8ad83092c79d50ee769547ee4bf1f
Mitchell Christ <xzmaru@gmail.com>
2022-01-23 22:30:46 -0800

    update schema

--- schemas/sticker.js
+++ schemas/sticker.js
@@ -9,61 +9,33 @@ export default {
 		{
 			name: 'image',
 			type: 'object',
-			options: { collapsible: true, collapsed: false, columns: 2, },
+			options: { collapsible: true, collapsed: false, columns: 2 },
 			fields: [
-				{ name: 'face', type: 'image', },
-				{ name: 'original', type: 'image', },
-				{ name: 'source', type: 'url', },
-				{ name: 'embed', type: 'text', rows: 2, },
-			],
+				{ name: 'face', type: 'image' },
+				{ name: 'original', type: 'image' },
+				{ name: 'source', type: 'url' }
+			]
 		},
-		{
-			name: 'emotion',
-			type: 'object',
-			options: { collapsible: true, collapsed: false, columns: 2, },
-			fields: [
-				{
-					name: 'emoji',
-					title: 'Emojis',
-					type: 'string',
-				},
-			],
-		},
-		{
-			name: 'meta',
-			title: 'Meta data',
-			type: 'object',
-			options: { collapsible: true, collapsed: false, columns: 2, },
-			fields: [
-				{ name: 'date', type: 'date', },
-				{ name: 'description', type: 'text', rows: 6 },
-			],
-		},
+		{ name: 'emojis', type: 'string' },
+		{ name: 'date', type: 'date' }
 	],
 	preview: {
 		select: {
-			emotion: 'emotion',
-			meta: 'meta',
-			media: 'image.face',
-		},
-		prepare({ emotion, meta, ...selection }) {
-			return {
-				title: emotion.emoji || '😶',
-				subtitle: [meta?.date, meta?.description].filter(Boolean).join` — `,
-				...selection,
-			};
-		},
+			title: 'emojis',
+			subtitle: 'date',
+			media: 'image.face'
+		}
 	},
 	orderings: [
 		{
 			title: 'Date',
 			name: 'dateDesc',
-			by: [{ field: 'meta.date', direction: 'desc' }]
+			by: [{ field: 'date', direction: 'desc' }]
 		},
 		{
 			title: 'Emoji',
 			name: 'emojiAsc',
-			by: [{ field: 'emotion.emoji', direction: 'asc' }]
-		},
+			by: [{ field: 'emojis', direction: 'asc' }]
+		}
 	]
 }
