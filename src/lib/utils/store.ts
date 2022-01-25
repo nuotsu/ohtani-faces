f3e8351f537cb0855cb8f7ce3acae10ebfef2076
Mitchell Christ <xzmaru@gmail.com>
2022-01-25 00:37:39 -0800

    use generics

--- src/lib/utils/store.ts
+++ src/lib/utils/store.ts
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store'
-import type { Writable } from 'svelte/store'
 
-export const selectedEmoji: Writable<Emoji | false> = writable(false)
+export const selectedEmoji = writable<Emoji | false>(false)
 
-export const selectedSticker: Writable<Sticker | false> = writable(false)
+export const selectedSticker= writable<Sticker | false>(false)
