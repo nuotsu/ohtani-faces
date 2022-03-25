d95d5427f1a76761bc47f19fdd1989d4686aea30
Mitchell Christ <xzmaru@gmail.com>
2022-03-24 22:41:26 -0700

    add ad component

--- packages/web/src/routes/index.svelte
+++ packages/web/src/routes/index.svelte
@@ -1,17 +1,19 @@
-<Header />
-<EmojiList />
-<SelectedEmoji />
-<StickerList />
-<!-- <Footer /> -->
-<Details />
+<Header/>
+<EmojiList/>
+<SelectedEmoji/>
+<StickerList/>
+<Ad/>
+<Footer/>
+<Details/>
 
 <script>
 	import Header from '~/lib/Header.svelte'
 	import EmojiList from '~/lib/emoji/EmojiList.svelte'
 	import SelectedEmoji from '~/lib/emoji/SelectedEmoji.svelte'
 	import StickerList from '~/lib/sticker/StickerList.svelte'
+	import Ad from '~/lib/Ad.svelte'
+	import Footer from '~/lib/Footer.svelte'
 	import Details from '~/lib/details/Details.svelte'
-	// import Footer from '~/lib/Footer.svelte'
 	import '../app.css'
 </script>
 
