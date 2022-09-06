22af48a88cc18e34868493132986a54ec340cfaa
Mitchell Christ <xzmaru@gmail.com>
2022-09-05 20:04:06 -0700

    update schema

--- packages/sanity/desk/structure-config.js
+++ packages/sanity/desk/structure-config.js
@@ -1,12 +1,10 @@
 import S from '@sanity/desk-tool/structure-builder'
 import { singleton, list } from './helpers'
 import { VscServerProcess, VscSmiley } from 'react-icons/vsc'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 export default () => S.list()
 	.title('Content')
 	.items([
 		singleton('Site', 'site').icon(VscServerProcess),
 		list('Stickers', 'sticker').icon(VscSmiley),
-		singleton('Player info', 'player').icon(BsFillPersonLinesFill),
 	])
--- packages/sanity/schemas/schema.js
+++ packages/sanity/schemas/schema.js
@@ -4,14 +4,12 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // documents
 import site from './documents/site'
 import sticker from './documents/sticker'
-import player from './documents/player'
 
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([
-    // documents
-    site,
-    sticker,
-    player,
-  ]),
+	name: 'default',
+	types: schemaTypes.concat([
+		// documents
+		site,
+		sticker,
+	]),
 })
